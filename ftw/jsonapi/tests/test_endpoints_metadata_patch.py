173a68f50aeb505cb4a128752bc75623c3f2fd83
Jonas Baumann <jone@jone.ch>
2015-10-12 18:36:29 +0300

    add test for patching DX objects.

--- ftw/jsonapi/tests/test_endpoints_metadata_patch.py
+++ ftw/jsonapi/tests/test_endpoints_metadata_patch.py
@@ -8,7 +8,7 @@ import json
 class TestMetadataPatchEndpoint(FunctionalTestCase):
 
     @browsing
-    def test_patch_title(self, browser):
+    def test_patch_title_AT(self, browser):
         self.grant('Manager')
         page = create(Builder('page').titled('The Page'))
 
@@ -18,4 +18,17 @@ class TestMetadataPatchEndpoint(FunctionalTestCase):
         self.assertDictContainsSubset(
             {'@url': '{}/the-page/api/metadata'.format(self.portal.absolute_url()),
              'title': 'The Very First Page'},
+            browser.json)
+
+    @browsing
+    def test_patch_title_DX(self, browser):
+        self.grant('Manager')
+        item = create(Builder('dx item').titled(u'The Item'))
+
+        browser.login().webdav('PATCH', item, view='api/metadata',
+                               data={'data': json.dumps({'title': 'The best item in town'})})
+        self.assertEquals(200, browser.response.status_code)
+        self.assertDictContainsSubset(
+            {'@url': '{}/the-item/api/metadata'.format(self.portal.absolute_url()),
+             'plone.app.dexterity.behaviors.metadata.IBasic.title': 'The best item in town'},
             browser.json)
