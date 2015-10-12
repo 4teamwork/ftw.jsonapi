a4d930d89ff12f45b02c7ff0f6c6b3640bf7c647
Jonas Baumann <jone@jone.ch>
2015-10-12 15:33:55 +0300

    have a Endpoint superclass.

---
+++ ftw/jsonapi/endpoints/__init__.py
@@ -0,0 +1,10 @@
+from ftw.jsonapi.interfaces import IAPIEndpoint
+from zope.interface import implements
+
+
+class Endpoint(object):
+    implements(IAPIEndpoint)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
--- ftw/jsonapi/endpoints/metadata.py
+++ ftw/jsonapi/endpoints/metadata.py
@@ -1,25 +1,20 @@
-from ftw.jsonapi.interfaces import IAPIEndpoint
+from ftw.jsonapi.endpoints import Endpoint
 from ftw.jsonapi.interfaces import IGET
 from ftw.jsondump.interfaces import IJSONRepresentation
 from zope.component import adapts
 from zope.component import getMultiAdapter
-from zope.interface import implements
 from zope.interface import Interface
 
 
-class GetMetadata(object):
+class GetMetadata(Endpoint):
     """Returns the JSON representation of the metadata of the object.
     """
 
-    implements(IAPIEndpoint)
     adapts(Interface, IGET)
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
 
     def __call__(self):
         json_representation = getMultiAdapter(
             (self.context, self.request),
             IJSONRepresentation)
+
         return json_representation.json()
